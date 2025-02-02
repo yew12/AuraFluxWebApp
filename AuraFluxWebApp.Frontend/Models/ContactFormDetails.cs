009804b4a418d1599c2c977a9f8544027c5e2867
yew12 <73267321+yew12@users.noreply.github.com>
2025-02-01 21:58:44 -0500

    initail commit

---
+++ AuraFluxWebApp.Frontend/Models/ContactFormDetails.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AuraFluxWebApp.Frontend.Models;
+
+public class ContactFormDetails
+{
+    [Required(ErrorMessage = "Please enter your name.")]
+    public string Name { get; set; } = "";
+
+    [Required(ErrorMessage = "Please enter a valid email address.")]
+    [EmailAddress]
+    public string Email { get; set; } = "";
+
+    [Required(ErrorMessage = "Please enter a message.")]
+    public string Message { get; set; } = "";
+}
